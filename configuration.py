d0c1119ca339074ef4d448e935019ea926d6f02b
ArtemZakharow <artzaharow@yandex.ru>
2024-02-25 16:58:28 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://4c7c2c5d-b6ef-42eb-a923-db19e8c64868.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Артем",
+    "lastName": "Захаров",
+    "address": "Pio, 137 apt.",
+    "metroStation": 204,
+    "phone": "+7 921 365 76 67",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-02",
+    "comment": "Тест",
+    "color": [
+        "BLACK"
+    ]
+}
